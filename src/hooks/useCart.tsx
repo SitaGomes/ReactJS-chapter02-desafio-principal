b59956a2acdcf2069edcfc528eb9cc3eb7dd1009
SitaGomes <ansitagomes@gmail.com>
2021-09-28 12:37:42 +0100

    Add: Cart completed

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -117,7 +117,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
       const {data: stock} = await api.get<Stock>(`stock/${productId}`)
 
-      if (amount >= stock.amount || amount <= 0) {
+      if (amount > stock.amount || amount <= 0) {
         toast.error('Quantidade solicitada fora de estoque');
         return
       }
@@ -133,8 +133,6 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
       setCart(updatedCart)
       localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
-
-
       
       //TODO: Know if it's an incrementation or a decrementation
       //TODO: Check if the product exists in general
